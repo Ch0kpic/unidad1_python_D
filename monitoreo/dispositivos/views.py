629f64d7f42cd1e6090d56144a560ee764ff627c
Dylan <dylanjbs01@gmail.com>
2025-08-24 22:29:38 -0400

    primer commit

---
+++ monitoreo/dispositivos/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def panel_dispositivos(request):
+    dispositivos = [
+        {"nombre": "Sensor Temperatura", "consumo": 110},
+        {"nombre": "Medidor Solar", "consumo": 120},
+        {"nombre": "Sensor Movimiento", "consumo": 30},
+        {"nombre": "Calefactor", "consumo": 200},
+    ]
+
+    consumo_maximo = 100
+
+    dispositivos_rojos = [d for d in dispositivos if d["consumo"] > consumo_maximo]
+    contador_rojos = len(dispositivos_rojos)
+
+    return render(request, "dispositivos/panel.html" , {
+        "dispositivos": dispositivos,
+        "consumo_maximo": consumo_maximo,
+        "contador_rojos": contador_rojos,
+    })
